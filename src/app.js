890f6d2543c5d8f6e2f3b09f1c15ba775b539e6e
Patrick Dal Molin Rezende <patrick.dm.rezende@outlook.com>
2021-05-18 23:20:53 -0300

    chore: update config files
    add consign lib
    add config/middlewares file

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const app = require('express')();
+const consign = require('consign');
 
-const app = express();
-
-app.use(express.json());
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send();
