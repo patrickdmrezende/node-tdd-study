f889c0ec6f6ace655025d16fecd7f10dc477c0fe
Patrick Dal Molin Rezende <patrick.dm.rezende@outlook.com>
2021-05-19 22:06:57 -0300

    feat: add knex and update test
    add knex and knex connection
    update user tests suite

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,18 +1,18 @@
 const request = require('supertest');
-const app = require('../src/app');
+const app = require('../../src/app');
 
 test('should list all users', () => {
   return request(app).get('/users')
     .then((res) => {
       expect(res.status).toBe(200);
-      expect(res.body).toHaveLength(1);
-      expect(res.body[0]).toHaveProperty('name', 'Mary Jane');
+      expect(res.body.length).toBeGreaterThan(0);
     });
 });
 
 test('should insert user with success', () => {
+  const mail = `${Date.now()}@mail.com`;
   return request(app).post('/user')
-    .send({ name: 'Han Solo', mail: 'hansolo@mail.com' })
+    .send({ name: 'Han Solo', mail, passwd: '123456' })
     .then((res) => {
       expect(res.status).toBe(201);
       expect(res.body.name).toBe('Han Solo');
